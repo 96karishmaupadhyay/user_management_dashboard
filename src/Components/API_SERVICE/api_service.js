1fe68a0e6086dc9f65bf769219dc2b1704feb3ff
96karishmaupadhyay <karishmaupadhyay1996@gmail.com>
2024-11-27 11:26:21 +0530

    api_service code committed

---
+++ src/Components/API_SERVICE/api_service.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const URL="https://jsonplaceholder.typicode.com/users"
+export const getUsers=async()=>{
+    try {
+        const response=await axios.get(URL);
+        return response.data ;
+    } catch (error) {
+        throw new Error("url is unable to fetch data")
+    }
+    
+}
+
+export const addUser=async(user)=>{
+    try {
+        const response=await axios.post(URL,user);
+        return response.data ;
+    } catch (error) {
+        throw new Error("url is unable to add data")
+    }
+    
+}
+
+export const deleteUser=async(id,user)=>{
+    try {
+        const response=await axios.delete(`${URL}${id}`,user);
+        return response.data ;
+    } catch (error) {
+        throw new Error("url is unable to delete user")
+    }
+    
+}
+
+export const updateUser=async(id,user)=>{
+    try {
+        const response=await axios.put(`${URL}${id}`,user);
+        return response.data ;
+    } catch (error) {
+        throw new Error("url is unable to update user")
+    }
+    
+}
